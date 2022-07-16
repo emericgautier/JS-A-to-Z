47b53c81aa21eb4ee6b9f2dbd33cbb003ce00e12
emericgautier <emericgtr@gmail.com>
2022-07-16 10:26:27 +0200

    click event

---
+++ lessons/index.js
@@ -0,0 +1,27 @@
+// SELECTEURS
+// document.querySelector("").style.background = "yellow";
+// const baliseHTML = document.querySelector("h4");
+
+// Click event
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "green";
+});
+
+btn2.addEventListener("click", () => {
+  response.classList.add("show-response");
+  response.style.background = "red";
+});
+
+/* Priorities */
+/* <div> > #id > .class > baliseHTML */
+/* !important */
