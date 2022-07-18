60ed32b2854717a9852616656cc24d4f53f9a9e7
emericgautier <emericgtr@gmail.com>
2022-07-18 20:38:41 +0200

    logic

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,23 @@
-alert("it works!");
+const buttons = document.querySelectorAll(".btn");
+const result = document.getElementById("result");
+const equal = document.getElementById("equal");
+
+// console.log(buttons);
+// console.log(result);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    result.textContent += e.target.id;
+  });
+});
+
+// console.log(equal);
+equal.addEventListener("click", () => {
+  //   console.log(equal);
+  result.textContent = eval(result.textContent);
+});
+
+// console.log(clear);
+clear.addEventListener("click", () => {
+  result.textContent = "";
+});
