730e68d78039a93a57bbcf5484c6322525f19901
emericgautier <emericgtr@gmail.com>
2022-07-22 11:16:23 +0200

    Math.rendom, setProperty, timer, event

--- bubble/index.js
+++ bubble/index.js
@@ -1,13 +1,28 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-// console.log(Math.random() * 200 + 100);
-const size = Math.random() * 200 + 100 + "px";
-// console.log(size);
+  // console.log(Math.random() * 200 + 100);
+  const size = Math.random() * 200 + 100 + "px";
+  // console.log(size);
 
-bubble.style.height = size;
-bubble.style.width = size;
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+    bubble.remove();
+  });
+
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+setInterval(bubbleMaker, 1000);
