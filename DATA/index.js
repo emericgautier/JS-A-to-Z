27e319d7f7aba4d68b2218304d2be6bb63a9239f
emericgautier <emericgtr@gmail.com>
2022-07-29 17:49:25 +0200

    arrays

--- DATA/index.js
+++ DATA/index.js
@@ -4,3 +4,49 @@ let number = 25;
 let cool = true;
 let maVariable; // type Undefined
 // console.log(typeof boolean);
+
+// Tableaux
+let array = ["Bordeaux", "Toulouse", "Nantes"];
+// console.log(array[0][3]);
+
+let array2 = ["Bordeaux", 24, true, [1, 2], { name: "Denis", job: "Artist" }];
+// console.log(array2[4].job);
+
+// Object
+let object = {
+  pseudo: "Denis",
+  age: 33,
+  technos: ["Javascript", "React", "NodeJs"],
+  admin: false,
+};
+
+// ajouter un élément dans un objet
+// object.adesse = "22 Rue du Code";
+
+// mettre à jour une valeur
+// object.age = 35;
+
+// console.log(object);
+
+// BDD
+let data = [
+  {
+    pseudo: "Denis",
+    age: 33,
+    technos: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Samia",
+    age: 24,
+    technos: ["CSS", "React", "NodeJs"],
+    admin: false,
+  },
+  {
+    pseudo: "Nikola",
+    age: 42,
+    technos: ["Php", "React", "NodeJs"],
+    admin: true,
+  },
+];
+// console.log(data[2].technos[1]);
