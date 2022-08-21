9f1e00a3973454994438adb601e69848ed78100a
emericgautier <emericgtr@gmail.com>
2022-08-21 17:25:28 +0200

    unit testing

---
+++ BONUS/test-js/fusionArray.test.js
@@ -0,0 +1,14 @@
+// importer la fonction
+const fusionArray = require("./fusionArray");
+
+test("fusionne 2 tableaux", () => {
+  let arr1 = [1, 2];
+  let arr2 = [3, 4];
+
+  // methode de comparaison toEqual
+  expect(fusionArray(arr1, arr2)).toEqual([1, 2, 3, 4]);
+  expect(fusionArray(arr1, arr2)).not.toBe([
+    [1, 2],
+    [3, 4],
+  ]);
+});
