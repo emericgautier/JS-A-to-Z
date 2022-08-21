860ef5b5c43d1a7ef2d14948c489bfc2defda21a
emericgautier <emericgtr@gmail.com>
2022-08-21 13:59:48 +0200

    fix event drop

--- BONUS/drag-and-drop/index.js
+++ BONUS/drag-and-drop/index.js
@@ -12,9 +12,11 @@ document.addEventListener("dragover", (e) => {
 
 // e.target est la division par dessus laquelle on est, pour relacher
 document.addEventListener("drop", (e) => {
-  if (e) e.preventDefault(e);
-  //   console.log(e.target);
-  e.target.appendChild(item);
+  if (e.target.getAttribute("data-draggable") == "target") {
+    e.preventDefault(e);
+    //   console.log(e.target);
+    e.target.appendChild(item);
+  }
 });
 
 // quand on a fini le drag, je te passe null
