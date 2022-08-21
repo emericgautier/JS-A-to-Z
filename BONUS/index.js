053020467246746206d292aab68bfb1b979993fe
emericgautier <emericgtr@gmail.com>
2022-08-21 11:09:34 +0200

    canvas

---
+++ BONUS/index.js
@@ -0,0 +1,28 @@
+// CANVAS
+//-------
+
+function draw() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d"); // ma balise je te donne un context
+
+  ctx.fillStyle = "rgb(200, 0, 0)"; // crée un élément à remplir
+  ctx.fillRect(50, 50, 50, 150); // donner les dimensions, sur l'axe de x: 50, y: 50, dim: 50x150
+
+  ctx.fillStyle = "rgba(0,100,200,0.5)"; // opacity: 0.5
+  ctx.fillRect(100, 30, 100, 50);
+
+  ctx.fillStyle = "rgba(100,100,200, 0.5)";
+  ctx.fillRect(10, 25, 100, 100);
+  ctx.clearRect(35, 25, 40, 40); // carré dont il ne peut pas avoir de carré à l'intérieur
+  ctx.strokeRect(125, 75, 50, 50); // juste des lignes qui se déssine
+
+  ctx.fillStyle = "rgba(200,100,200, 0.8)";
+  ctx.beginPath(); // démarrer une forme géométrique
+  ctx.moveTo(180, 150); // 180x150
+  ctx.lineTo(100, 75); // d'abord faire une ligne de 100x75
+  ctx.lineTo(100, 150); // d'abord faire une ligne de 100x150
+  ctx.fill();
+}
+
+// on se joue cette function quand la page est chargé
+window.addEventListener("load", draw);
